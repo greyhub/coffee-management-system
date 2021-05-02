939e545c107f42ec6ed7053d2e81f3f4e23988f7
vietnha <vietnha@vng.com.vn>
2021-05-02 10:51:45 +0700

    Fix xxx

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import env from './env';
 import logger from './_base/log/logger4js';
 import ormInit from './ormConnection';
 
+logger.debug("run createExpressApp");
 async function createExpressApp () {
   await ormInit();
   app.set('port', process.env.PORT || 80);
@@ -13,8 +14,8 @@ async function createExpressApp () {
 
   // const server = http.createServer(app);
   // server.listen(process.env.PORT);
-  app.listen(process.env.PORT);
+  app.listen(process.env.PORT || 80);
   // server.listen(process.env.PORT || 80, '0.0.0.0');
-  logger.debug(`Service is listening on PORT=${env.PORT}`);
+  logger.debug(`Service is listening on PORT=${process.env.PORT}`);
 }
 createExpressApp();

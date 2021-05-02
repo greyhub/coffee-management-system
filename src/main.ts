cb1c79ce977631f3eba53dc704249f7a31443bb3
vietnha <vietnha@vng.com.vn>
2021-05-02 09:44:56 +0700

    Log

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import ormInit from './ormConnection';
 
 async function createExpressApp () {
   await ormInit();
-  app.set('port', env.PORT || 80);
+  app.set('port', process.env.PORT || 80);
+
+  logger.debug("PORT-process" + process.env.PORT);
+  logger.debug("PORT-env" + env.PORT);
 
   const server = http.createServer(app);
-  server.listen(env.PORT || 80);
+  server.listen(process.env.PORT || 80);
   console.table([{ Author: '@vietnha', Contact: 'viet.nha173465@sis.hust.edu.vn', Server: 'localhost:'+env.PORT}]);
   logger.debug(`Service is listening on PORT=${env.PORT}`);
 }

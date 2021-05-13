09a9bad852195f91646d27ef8f751042019574c7
greyhub <luongnv.grey@gmail.com>
2021-05-13 10:57:25 +0700

    Update routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,6 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
-import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
@@ -16,8 +10,16 @@ import Statistics from "views/Statistics/Statistics.js";
 // core components/views for RTL layout
 import RTLPage from "views/RTLPage/RTLPage.js";
 import Purchasing from "views/Purchasing/Purchasing.js"
+import Login from "views/Login/Login";
 
 const dashboardRoutes = [
+  // {
+  //   path: "/login",
+  //   name: "Login",
+  //   rtlName: "",
+  //   component: Login,
+  //   layout: "/admin"
+  // },
   {
     path: "/dashboard",
     name: "Dashboard",
